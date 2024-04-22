71f9c2c4ead867b2a9a99a405ffd857d56f6070d
pythonwarrior42 <g.luchita@protonmail.com>
2024-04-22 13:10:34 +0100

    modified aiports

--- flex/07-flex-layout-2/aiports.py
+++
@@ -1,6 +0,0 @@
-airports = ["Barcelona", "Dublin", "London", "Paris", "Prague", "Rey", "Rome"]
-codes = ["BCN", "DUB", "LIS", "LHR", "CDG", "PRG", "RKV", "FCO"]
-code = str(input("input code"))
-index = codes.index(code)
-print(airports[index])
-
